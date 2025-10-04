96cfea1f70d60f2a32ed6542ef617bf4353d8ccd
Mezzofloat <skull.wizard.2299@gmail.com>
2025-10-04 19:16:35 -0400

    added thank the bus driver minigame

--- get_off_bus.gd
+++ get_off_bus.gd
@@ -2,14 +2,26 @@ extends Button
 
 var doorsOpen = false
 var offBus = false
+var thankable = false
 @export var node: Sprite2D
 @export var getOffBusAnim: AnimationPlayer
+@export var thanksBubble: Sprite2D
+@onready var cooldown: Timer = $Timer
 
 signal buttonPress
 
 func _process(delta: float) -> void:
-	if (Input.is_action_pressed("space_click")):
+	if (Input.is_action_pressed("space_click") && !offBus):
 		_pressed()
+		cooldown.start()
+	elif (Input.is_action_pressed("space_click") && thankable):
+		_thank_bus_driver()
+
+func _make_thankable() -> void:
+	thankable = true
+
+func _thank_bus_driver() -> void:
+	thanksBubble.visible = true
 
 func _pressed() -> void:
 	if doorsOpen && !offBus:
---
+++ get_off_bus_minigame.gd
@@ -0,0 +1,38 @@
+
+extends Node2D
+var doorsOpen = false
+var offBus = false
+@onready var getOffBusAnim: AnimationPlayer = $AnimationPlayer
+@onready var stoptimer = $TimerToDoorOpen
+signal buttonPress
+
+func _process(delta: float) -> void:
+	if (Input.is_action_pressed("space_click")):
+		_on_button_pressed()
+
+func _pressed() -> void:
+	pass
+
+func _on_stop() -> void:
+	doorsOpen = true
+
+func _on_timeout() -> void:
+	doorsOpen = false
+
+
+func _on_button_pressed() -> void:
+	if doorsOpen && !offBus:
+		buttonPress.emit()
+		getOffBusAnim.play("player_out_of_bus")
+		offBus = true
+		stoptimer.paused = true
+		await getOffBusAnim.animation_finished
+		print('hi')
+
+
+func _on_timer_to_stop_timeout() -> void:
+	doorsOpen = true
+
+
+func _on_timer_to_door_open_timeout() -> void:
+	gman.endgame('Missed Your Stop')
