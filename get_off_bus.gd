61320fffb5e0299bb46dc7cf25a06a3fa468dd84
Mezzofloat <skull.wizard.2299@gmail.com>
2025-10-04 14:42:55 -0400

    made it return false based on time

--- get_off_bus.gd
+++ get_off_bus.gd
@@ -1,16 +1,9 @@
 extends Button
 
 var b = true
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
 	
 func _pressed() -> void:
-	b = !b
 	print(b)
+
+func _on_timeout() -> void:
+	b = false
