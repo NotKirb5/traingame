f7427933db00be93f5dca127ee7da0f4426005e1
Mezzofloat <skull.wizard.2299@gmail.com>
2025-10-04 14:21:06 -0400

    made get off bus finally work

--- get_off_bus.gd
+++ get_off_bus.gd
@@ -1,5 +1,6 @@
-extends TextureButton
+extends Button
 
+var b = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,3 +10,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+	
+func _pressed() -> void:
+	b = !b
+	print(b)
